46c00d3be12aaca2640010985ab287dd6995e007
Richard Senger <richard.senger27@icloud.com>
2024-03-29 00:16:42 +0100

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import '@/globals.css'
+import { useEffect, useState } from 'react'
+import { useSessionStorage } from 'usehooks-ts'
+import CriterionTable from './components/CriterionTable'
+import ResultTable from './components/ResultTable'
+
+function App() {
+  const [columns, setColumns] = useState<Column[]>([])
+  const [rows, setRows] = useState<Row[]>([])
+  
+  const [tableProps, setTableProps] = useState<TableProps>({
+    columns: [],
+    setColumns: () => {},
+    rows: [],
+    setRows: () => {},
+  })
+
+  const [storedColumns, setStoredColumns] = useSessionStorage<Column[]>('columns', columns)
+  const [storedRows, setStoredRows] = useSessionStorage<Row[]>('rows', rows)
+  
+  useEffect(() => {
+    setColumns(storedColumns)
+    setRows(storedRows)
+  }, [])
+  useEffect(() => {
+    setTableProps({
+      columns,
+      setColumns,
+      rows,
+      setRows
+    })
+    // console.log('columns:', columns)
+    // console.log('rows:', rows)
+    setStoredColumns(columns)
+    setStoredRows(rows)
+  }, [columns, rows])
+  return (
+    <>
+      <CriterionTable {...tableProps}/>
+      <ResultTable {...tableProps}/>
+    </>
+  )
+}
+
+export type Row = {
+  title: string,
+  [key: string]: string
+}
+
+export type Column = {
+  title: string,
+  benefitial: boolean
+}
+
+export type TableProps = {
+  columns: Column[],
+  setColumns: Function,
+  rows: Row[],
+  setRows: Function
+}
+
+export default App
